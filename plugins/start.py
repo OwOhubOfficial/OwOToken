ebdafcd5668d22dfbf3733415d4741cc9fad3301
hoobooji <162422722+hoobooji@users.noreply.github.com>
2024-04-21 15:35:08 -0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -121,8 +121,8 @@ async def start_command(client: Client, message: Message):
 
         elif verify_status['is_verified']:
             reply_markup = InlineKeyboardMarkup(
-                [[InlineKeyboardButton("ð About Me", callback_data="about"),
-                  InlineKeyboardButton("ð Close", callback_data="close")]]
+                [[InlineKeyboardButton("👽 About Me", callback_data="about"),
+                  InlineKeyboardButton("🔒 Close", callback_data="close")]]
             )
             await message.reply_text(
                 text=START_MSG.format(
